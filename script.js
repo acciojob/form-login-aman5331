e7b5a38edbcab8d846407ba20c186365b1ec36b2
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-25 16:23:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+
+  alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
 
 }
